e445c93ff625b080d234ee022eb8f48154cdf21d
GoodInc2k@gmail.com <GoodInc2k@gmail.com@fb96253e-b5d4-3706-ed20-4aabb2778203>
2014-08-05 21:07:24 +0000

    Adding horizontal main layout. Few tweaks in vertical main layout.

--- trunk/health-diary/src/goodinc2k/health_diary/MainActivity.java
+++ trunk/health-diary/src/goodinc2k/health_diary/MainActivity.java
@@ -1,15 +1,29 @@
 package goodinc2k.health_diary;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {         
+	public void onCreate(Bundle savedInstanceState) {
 
-       super.onCreate(savedInstanceState);    
-       setContentView(R.layout.activity_main);
-       //rest of the code
-   }
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		TextView versionText = (TextView) findViewById(R.id.text_version);
+		PackageInfo pInfo = null;
+		try {
+			pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		if (pInfo != null) {
+			String versionName = getString(R.string.version, pInfo.versionName);
+			versionText.setText(versionName);
+		}
+	}
 }
