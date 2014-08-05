019709537c1fbae40cbbda684bd5536d076188bb
GoodInc2k@gmail.com <GoodInc2k@gmail.com@fb96253e-b5d4-3706-ed20-4aabb2778203>
2014-08-05 07:42:39 +0000

    Initial main screen layout for vertical orientation

---
+++ trunk/health-diary/src/goodinc2k/health_diary/MainActivity.java
@@ -0,0 +1,15 @@
+package goodinc2k.health_diary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {         
+
+       super.onCreate(savedInstanceState);    
+       setContentView(R.layout.activity_main);
+       //rest of the code
+   }
+}
